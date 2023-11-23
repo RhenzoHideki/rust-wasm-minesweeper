6a9c938bd32bca38552c7b4c8e4eeec93f50bff7
RhenzoHideki <hidekikajikawa@gmail.com>
2023-11-23 20:59:49 -0300

    Minesweeper initial steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::collections::HashSet;
+
+use rand::{thread_rng, Rng};
+
+type Position = (usize, usize);
+struct Minesweeper{
+    // Maybe change width and height for a L x L type , instead of using 2 variables 
+    width: usize,
+    height: usize,
+
+    open_field: HashSet<Position>,
+    mines: HashSet<Position>,
+    flags: HashSet<Position>,
+
+}
+impl Minesweeper {
+    pub fn new(width: usize , height: usize, mine_count: usize) -> Minesweeper{
+        Minesweeper {
+            width,
+            height,
+            open_field: HashSet::new(), 
+            mines: Self::generate_mines(width,height,mine_count),
+            flags, 
+        }
+    }
+
+    fn generate_mines(width: usize , height: usize, mine_count: usize) -> HashSet<Position>{
+        let mut mines = HashSet::new();
+
+        let mut rng = thread_rng();
+        
+        while mines.len() < mine_count {
+
+            let x = rng.gen_range(0..width);
+            let y = rng.gen_range(0..height);
+
+            mines.insert((x,y));
+        }
+        mines
+
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
